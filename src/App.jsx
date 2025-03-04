e5d3024af51bdaf9b69235547d283c77a4a56bca
Gamson3 <gamson3@yahoo.com>
2025-03-04 14:28:29 +0100

    Fix GitHub Pages routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@
 import "./css/App.css";
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Use HashRouter since Since GitHub Pages only serves static files
 import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <MovieProvider>
+      <Router>  {/* Wrap everything inside HashRouter */}
       <NavBar />
       <main className="main-content">
         <Routes>
@@ -18,6 +20,7 @@ function App() {
           <Route path="/favourites" element={<Favourites />} />
         </Routes>
       </main>
+      </Router>
     </MovieProvider>
   );
 }
